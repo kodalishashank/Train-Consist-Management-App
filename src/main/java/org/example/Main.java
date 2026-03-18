eea648c0c05c44ba29a1ba19f7c201a579acc5ca
AG-0015 <sk6084@srmist.edu.in>
2026-03-18 15:19:10 +0530

    UC6: Map Bogie to Capacity (HashMap)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
@@ -112,6 +114,22 @@ public class Main {
         System.out.println("Note:");
         System.out.println("LinkedHashSet preserves insertion order and removes duplicates automatically.\n");
 
-        System.out.println("UC5 formation setup completed...");
+        System.out.println("UC5 formation setup completed...\n");
+
+        System.out.println("------------------------------------------------");
+        System.out.println(" UC6 Map Bogie to Capacity (HashMap) ");
+        System.out.println("------------------------------------------------\n");
+
+        Map<String, Integer> bogieCapacity = new HashMap<>();
+        bogieCapacity.put("First Class", 24);
+        bogieCapacity.put("Cargo", 120);
+        bogieCapacity.put("Sleeper", 72);
+        bogieCapacity.put("AC Chair", 56);
+
+        System.out.println("Bogie Capacity Details:\n");
+        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+        System.out.println("\nUC6 bogie-capacity mapping completed...");
     }
 }
